4e5a379e49c9a1d16c2dc444ddce25d9f42392f1
afwerar <1296468573@qq.com>
2022-02-28 23:09:49 +0800

    [TD-13062]<fix>: file system add stream.

--- tsim/src/simExe.c
+++ tsim/src/simExe.c
@@ -22,7 +22,7 @@ void simLogSql(char *sql, bool useSharp) {
   sprintf(filename, "%s/sim.sql", simScriptDir);
   if (pFile == NULL) {
     // fp = fopen(filename, "w");
-    pFile = taosOpenFile(filename, TD_FILE_CTEATE | TD_FILE_WRITE | TD_FILE_TRUNC);
+    pFile = taosOpenFile(filename, TD_FILE_CTEATE | TD_FILE_WRITE | TD_FILE_TRUNC | TD_FILE_STREAM);
     if (pFile == NULL) {
       fprintf(stderr, "ERROR: failed to open file: %s\n", filename);
       return;
@@ -770,7 +770,7 @@ bool simExecuteRestfulCmd(SScript *script, char *rest) {
   char  filename[256];
   sprintf(filename, "%s/tmp.sql", simScriptDir);
   // fp = fopen(filename, "w");
-  pFile = taosOpenFile(filename, TD_FILE_CTEATE | TD_FILE_WRITE | TD_FILE_TRUNC);
+  pFile = taosOpenFile(filename, TD_FILE_CTEATE | TD_FILE_WRITE | TD_FILE_TRUNC | TD_FILE_STREAM);
   if (pFile == NULL) {
     fprintf(stderr, "ERROR: failed to open file: %s\n", filename);
     return false;
